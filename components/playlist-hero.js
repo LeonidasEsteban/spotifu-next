7e84b3e539a19409a02ba3067139c0bfec7e07cb
Leonidas Esteban <leonidasesteban@gmail.com>
2021-07-05 16:43:10 -0500

    [add] playlist hero

---
+++ components/playlist-hero.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function PlaylistHero({ name = '', description, type, images = [],...props}) {
+  console.log(props)
+  return (
+    <div className="playlistHero">
+      <img src={images[0].url} alt="" width="232" height="232" />
+      <div className="playlistHero-content">
+        <p className="playlistHero-type">{type}</p>
+        <h1 className="playlistHero-name">{name}</h1>
+        {
+          description ?
+          <h2 className="playlistHero-description">{description}</h2>
+          : null
+        }
+      </div>
+    </div>
+  )
+}
