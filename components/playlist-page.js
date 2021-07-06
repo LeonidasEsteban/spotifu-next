163ba70e62424ad5380e3e7a6d2c35b01ae607fe
Leonidas Esteban <leonidasesteban@gmail.com>
2021-07-05 19:01:57 -0500

    [update] playlist render to react query

--- components/playlist-page.js
+++ components/playlist-page.js
@@ -5,27 +5,24 @@ import Track from "./track";
 import { getPlaylist, getPlaylistTracks } from "../services/playlist";
 import PlaylistHero from "./playlist-hero";
 import PlaylistTable from "./playlist-table";
+import { useQuery } from "react-query";
 
 export default function PlaylistPage() {
-  const [playlist, setPlaylist] = useState({});
-  const [tracks, setTracks] = useState([]);
-
   const router = useRouter();
-  useEffect(async () => {
-    const playlistId = router.query.id;
-    const dataPlaylist = await getPlaylist(playlistId);
-    const dataTracks = await getPlaylistTracks(playlistId);
-    setPlaylist(dataPlaylist);
-    setTracks(dataTracks.items);
-  }, [router.query.id]);
+  const playlistId = router.query.id;
+  const { data: playlist } = useQuery(router.query.id, () => getPlaylist(playlistId))
+  const { data: tracks, isFetched } = useQuery(router.query.id, () => getPlaylistTracks(playlistId))
+
   return (
     <>
       <PlaylistHero {...playlist} />
       <PlaylistTable>
         {
-          tracks.map((track, index) => (
-            <Track {...track} index={index + 1} />
+          isFetched ?
+          tracks.tracks.items.map((track, index) => (
+            <Track {...track} key={track.id} index={index + 1} />
           ))
+          : 'cargando...'
         }
       </PlaylistTable>
     </>
